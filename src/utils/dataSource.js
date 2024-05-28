6169d0f6c76327e187050654111e7f6bc942081f
Dannika <71364766+dannikate@users.noreply.github.com>
2024-05-28 15:10:41 +1000

    add data source
    desc: util module to hold path for json data file

---
+++ src/utils/dataSource.js
@@ -0,0 +1,12 @@
+/* 
+dataSource.js
+Description: 
+Return: 
+*/
+
+// update this with data set; must be full path, starting from the root directory 
+// const geoJsonUrl = "./src/data/dummy_data.json";
+
+const geoJsonUrl = "./src/data/saipanWells.json";
+
+export { geoJsonUrl }
