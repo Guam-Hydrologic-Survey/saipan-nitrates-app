a941f24e567c6bff57b4274d51df89281c117245
Dannika <71364766+dannikate@users.noreply.github.com>
2024-09-03 14:27:16 +1000

    wip files for LMap and SelectionView (v3)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ Description: Entry point for project. Appends all components to #app in index.ht
 import { About } from "./components/About.js";
 import { FullscreenModal } from "./components/FullscreenModal.js";
 import { NavBar } from "./components/NavBar.js";
-import { LMap } from "./components/LMap_v2.js";
+import { LMap } from "./components/LMap_v3.js";
 import { LargePlotModal } from "./components/Plot.js";
 
 document.getElementById("app").innerHTML = /*html*/ 
