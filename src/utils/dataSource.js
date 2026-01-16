67e40ad7a70e2fa2e8dbfd685e065567fb4d1ccd
sydneymaq <quintanillasydney@gmail.com>
2026-01-16 15:26:09 +1000

    saipan_nitrates_1.16.26

--- src/utils/dataSource.js
+++ src/utils/dataSource.js
@@ -7,6 +7,6 @@ Return:
 // update this with data set; must be full path, starting from the root directory 
 // const geoJsonUrl = "./src/data/dummy_data.json";
 
-const geoJsonUrl = "./src/data/saipan_nitrates_sq2026.json";
+const geoJsonUrl = "./src/data/saipan_nitrates_sq_2026.01.json";
 
 export { geoJsonUrl }
