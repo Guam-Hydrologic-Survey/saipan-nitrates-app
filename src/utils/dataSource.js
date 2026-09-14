4adac30de818169d42a302ecc25b4252a7bed830
sydneymaq <144676993+sydneymaq@users.noreply.github.com>
2026-09-14 13:55:23 +1000

    second commit, new trial nitrates, changed the stats to raw unrounded figures

--- src/utils/dataSource.js
+++ src/utils/dataSource.js
@@ -7,7 +7,7 @@ Return:
 // update this with data set; must be full path, starting from the root directory 
 // const geoJsonUrl = "./src/data/dummy_data.json";
 
-const geoJsonUrl = "./src/data/cnmi_nitrates_final_09102026.json";
+const geoJsonUrl = "./src/data/cnmi_nitrates_final_09142026.json";
 
 export { geoJsonUrl }
 
